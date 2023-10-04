ab289f8c385f58e69dd40ab78e567699526ad113
Patel Harsh <102842153+Harrshhpattell@users.noreply.github.com>
2023-10-04 19:27:11 +0530

    footer complete with responsive

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFacebook, faTwitter, faInstagram, faYoutube } from '@fortawesome/free-brands-svg-icons'
-import '../styles/Footer.css'
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebook,
+  faTwitter,
+  faInstagram,
+  faYoutube,
+} from "@fortawesome/free-brands-svg-icons";
+import "../styles/Footer.css";
 
 const Footer = () => {
   return (
@@ -15,25 +20,26 @@ const Footer = () => {
         </div>
         <div className="social-links">
           <a href="#">
-            <FontAwesomeIcon icon={faFacebook} className="social-icon"/>
+            <FontAwesomeIcon icon={faFacebook} className="social-icon" />
           </a>
           <a href="#">
-            <FontAwesomeIcon icon={faTwitter} className="social-icon"/>
+            <FontAwesomeIcon icon={faTwitter} className="social-icon" />
           </a>
           <a href="#">
-            <FontAwesomeIcon icon={faInstagram} className="social-icon"/>
+            <FontAwesomeIcon icon={faInstagram} className="social-icon" />
           </a>
           <a href="#">
-            <FontAwesomeIcon icon={faYoutube} className="social-icon"/>
+            <FontAwesomeIcon icon={faYoutube} className="social-icon" />
           </a>
         </div>
-        <div className="copyright">
-          Copyright © {new Date().getFullYear()} AnswerMe All Rights Reserved
-        </div>
-        <div className="copyright2">Made By Aditya Sharma</div>
       </div>
+      <hr />
+      <div className="copyright">
+        Copyright © {new Date().getFullYear()} AnswerMe All Rights Reserved
+      </div>
+      <div className="copyright2">Made By Aditya Sharma</div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
