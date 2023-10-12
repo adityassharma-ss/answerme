9386faf04fa5061028f61b0b6ffbe83be40ce0a8
Patel Harsh <102842153+Harrshhpattell@users.noreply.github.com>
2023-10-12 22:16:34 +0530

    update buttons

--- client/src/components/Answer.jsx
+++ client/src/components/Answer.jsx
@@ -35,11 +35,23 @@ const Answer = () => {
             onChange={(e) => setQuery(e.target.value)}
           />
           <div className="submit-clear-section">
-            <button type="submit" className="video-game-button" id="container">
-              Submit
+            <button class="btn-sumbit">
+              <svg
+                height="24"
+                width="24"
+                fill="#FFFFFF"
+                viewBox="0 0 24 24"
+                data-name="Layer 1"
+                id="Layer_1"
+                class="sparkle"
+              >
+                <path d="M10,21.236,6.755,14.745.264,11.5,6.755,8.255,10,1.764l3.245,6.491L19.736,11.5l-6.491,3.245ZM18,21l1.5,3L21,21l3-1.5L21,18l-1.5-3L18,18l-3,1.5ZM19.333,4.667,20.5,7l1.167-2.333L24,3.5,21.667,2.333,20.5,0,19.333,2.333,17,3.5Z"></path>
+              </svg>
+
+              <span class="text">Submit</span>
             </button>
-            <button className="video-game-button" onClick={handleClear}>
-              Clear
+            <button className="btn-clear" onClick={handleClear}>
+              <span>Clear</span>
             </button>
           </div>
         </form>
@@ -50,8 +62,8 @@ const Answer = () => {
         </div>
         <div className="conversation-bubble bot-bubble">
           <p>{answer}</p>
-          <button className="video-game-button" onClick={() => setAnswer("")}>
-            Clear
+          <button className="btn-clear" onClick={() => setAnswer("")}>
+            <span>Clear</span>
           </button>
         </div>
       </div>
